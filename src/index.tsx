2e1f82452f767ba94026d338404f27de52634db8
Jamison Chen <106208004@g.nccu.edu.tw>
2023-01-09 11:12:27 +0800

    Add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import MyRouter from "./router";
 import { store } from "./redux/store";
@@ -14,14 +15,19 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    // <React.StrictMode>
-    <Provider store={store}>
-        <BrowserRouter>
-            <MyRouter />
-        </BrowserRouter>
-    </Provider>
-    // </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <MyRouter />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
